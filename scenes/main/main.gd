16ad40f978c23e1d1e27f29a916b58a8b00fbe90
Haigsov <haigsouv@my.yorku.ca>
2024-06-03 23:09:54 -0400

    Created HUD

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 var max_enemies : int
+var lives : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	max_enemies = 5
-
+	max_enemies = 10
+	lives = 3
+	$HUD/LivesLabel.text = "X " + str(lives)
+	$HUD/EnemiesLabel.text = "X " + str(max_enemies)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -16,3 +20,5 @@ func _process(_delta):
 
 func _on_enemy_spawner_hit_p():
 	print("hit player")
+	lives -= 1
+	$HUD/LivesLabel.text = "X " + str(lives)
